60dee11a54dd299f1f4e9aa0e6ed9e9ca0cad7ff
Rayan-David <davidrayan8@gmail.com>
2026-05-14 20:44:03 -0300

    Cadastro de livro

--- index.py
+++ index.py
@@ -9,3 +9,8 @@ if "livros" not in st.session_state:
     
 #Cadastrar livro
 st.header("Cadastrar Livro")
+nome_livro = st.text_input("Nome do Livro")
+autor = st.text_input("Autor")
+
+#Botão cadastrar
+st.button("Cadastrar Livro")
